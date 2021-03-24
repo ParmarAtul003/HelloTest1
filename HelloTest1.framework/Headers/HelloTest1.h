4b53f00fa3e5801ba7df8841235100568655c3a4
Atul Parmar <atul.parmar@spec-india.com>
2021-03-24 13:03:49 +0530

    add framework

---
+++ HelloTest1.framework/Headers/HelloTest1.h
@@ -0,0 +1,18 @@
+//
+//  HelloTest1.h
+//  HelloTest1
+//
+//  Created by Atul Parmar on 24/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelloTest1.
+FOUNDATION_EXPORT double HelloTest1VersionNumber;
+
+//! Project version string for HelloTest1.
+FOUNDATION_EXPORT const unsigned char HelloTest1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloTest1/PublicHeader.h>
+
+
